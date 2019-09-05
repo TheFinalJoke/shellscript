c0d338f8f209f9ae70bb0f8e6e36d4c273e92365
nickshorter <nickshorter@virtmanager.homelab>
2019-09-04 21:03:28 -0500

    modifying kvm

--- server/installingkvm.sh
+++ server/installingkvm.sh
@@ -5,7 +5,7 @@ exit 1
 fi
 
 #installing kvm
-echo "Installing KVM
+echo "Installing KVM"
 dnf install qemu-kvm libvirt libvirt-python libguestfs-tools virt-install -y >> /dev/null
 
 echo "Enabling libvirtd"
